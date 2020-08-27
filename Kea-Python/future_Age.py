98e5fbde3337c9c79c41c1b3e16231735b0a8306
Alek2750 <alek2750@gmail.com>
2020-08-27 12:21:30 +0200

    python øvelser

---
+++ Kea-Python/future_Age.py
@@ -0,0 +1,14 @@
+#print("The date is as follows:")
+#import datetime
+#now = datetime.datetime.now()
+#print (now.year, now.month, now.day)
+
+name = input("What is your name? ") 
+print(name)
+
+age = int(input("What is your age? ")) 
+print(age + 100)
+
+print("You are going to be age 100 on the year")
+print(2020-age+100)
+#print(now.year-age+100)
---
+++ Kea-Python/highest_number.py
@@ -0,0 +1,13 @@
+#Which number is biggest
+a = 5
+b = 9
+c = -2
+
+if (a >= b) and (a >= c):
+       biggest = a
+elif (b >= a) and (b >= c):
+   biggest = b
+else:
+   biggest = c
+
+print("The biggest number is", biggest)
