df91055fc31bd090fbd1102e5ca2a6bd0ce35b5e
Alek2750 <alek2750@gmail.com>
2020-09-07 15:00:04 +0200

    Update TCPservertest.py

--- Kea-Python/Mandatory_part1/TCPservertest.py
+++ Kea-Python/Mandatory_part1/TCPservertest.py
@@ -1,33 +1,50 @@
-from socket import *
-serverPort=6969
-serverSocket = socket(AF_INET, SOCK_STREAM)
-#Prepare a sever socket
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
-print ('the web server is up on port:',serverPort)
-print ('Ready to serve...')
-
+import socket
+ 
+HOST,PORT = '127.0.0.1', 6969
+ 
+my_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+my_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+my_socket.bind((HOST,PORT))
+my_socket.listen(1)
+ 
+print('Serving on port ',PORT)
+ 
 while True:
- #Establish the connection
- connectionSocket, addr = serverSocket.accept()
-
-try:
-
-    message = connectionSocket.recv(1024)
-    print (message,'::',message.split()[0],':',message.split()[1])
-    filename = message.split()[1]
-    print (filename,'||',filename[1:])
-    f = open(filename[1:])
-    outputdata = f.read()
-    print (outputdata)
- #Send one HTTP header line into socket
-    connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
-    connectionSocket.send(outputdata)
- #Send the content of the requested file to the client
-    for i in range(0, len(outputdata)):
-        connectionSocket.send(outputdata[i])
-        connectionSocket.close()
-except IOError:
- #Send response message for file not found
-    connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
-    connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
+    connection,address = my_socket.accept()
+    request = connection.recv(1024).decode('utf-8')
+    string_list = request.split(' ')     # Split request from spaces
+ 
+    method = string_list[0]
+    requesting_file = string_list[1]
+ 
+    print('Client request ',requesting_file)
+ 
+    myfile = requesting_file.split('?')[0] # After the "?" symbol not relevent here
+    myfile = myfile.lstrip('/')
+    if(myfile == ''):
+        myfile = 'index.html'    # Load index file as default
+ 
+    try:
+        file = open(myfile,'rb') # open file , r => read , b => byte format
+        response = file.read()
+        file.close()
+ 
+        header = 'HTTP/1.1 200 OK\n'
+ 
+        if(myfile.endswith(".jpg")):
+            mimetype = 'image/jpg'
+        elif(myfile.endswith(".css")):
+            mimetype = 'text/css'
+        else:
+            mimetype = 'text/html'
+ 
+        header += 'Content-Type: '+str(mimetype)+'\n\n'
+ 
+    except Exception as e:
+        header = 'HTTP/1.1 404 Not Found\n\n'
+        response = '<html><body><center><h3>Error 404: File not found</h3><p>Python HTTP Server</p></center></body></html>'.encode('utf-8')
+ 
+    final_response = header.encode('utf-8')
+    final_response += response
+    connection.send(final_response)
+    connection.close()
