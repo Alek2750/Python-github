056acbebef7345991f77cd8254519dbb60756fb0
Alek2750 <alek2750@gmail.com>
2020-09-09 15:57:45 +0200

    python sandbox

---
+++ Kea-Python/python_notes.py
@@ -0,0 +1,33 @@
+'''
+To see and remember the diffence
+'''
+
+# A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create list
+numbers = [1, 2, 3, 4, 5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+
+# A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+
+# Create tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
+
+# Single value needs trailing comma
+fruits2 = ('Apples',)
+
+
+# A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create set
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
+# Create dict
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30
+}
